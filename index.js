4389123bc8042bbf0372f76a03868af7035090a4
tahsin-99 <somethinglearn53@gmail.com>
2025-11-11 21:24:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://pet:tESTUASaX2G1k9rH@t-mongo.m4mnwdk.mongodb.net/?appName=T-mongo";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+
+    const db= client.db('pets-db')
+    const petsuppliessCollection=db.collection('pet-supplies')
+
+
+    app.get('/petsupplies',async(req,res)=>{
+      
+      const result=await petsuppliessCollection.find().toArray()
+      res.send(result)
+    })
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("hellow Brother");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
